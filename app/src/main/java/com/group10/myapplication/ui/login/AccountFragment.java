99ae702e3eef1c98155e8232244e0138daf3c554
mcwhyte <madelyncwhyte@gmail.com>
2021-11-15 11:23:08 -0500

    Added GPS capabilities to AccountFragment. Call GPSCurrency in to get the user's currency. Default is $. Can be used when creating the userAccount currency

--- app/src/main/java/com/group10/myapplication/ui/login/AccountFragment.java
+++ app/src/main/java/com/group10/myapplication/ui/login/AccountFragment.java
@@ -33,15 +33,45 @@ import java.security.NoSuchAlgorithmException;
 
 import timber.log.Timber;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Currency;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 /**
  * Fragment for user account creation.
  */
+
+//Call GPS Currency() to get the user's currency
+
 public class AccountFragment extends Fragment implements View.OnClickListener {
     private EditText mEtUsername;
     private EditText mEtPassword;
     private EditText mEtConfirm;
 
     private UserAccountViewModel mUserAccountViewModel;
+
+
+	LocationManager locationManager;
+	Context mContext;
+	Location location;
 
     private final String TAG = getClass().getSimpleName();
 
@@ -167,4 +197,134 @@ public class AccountFragment extends Fragment implements View.OnClickListener {
 		});
 	}
 
-}
+	//Get the currency code:
+
+		public String GPSCurrency() {
+			mContext = getActivity();
+			//Create location manager object
+
+
+			locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+
+			location = new Location(LocationManager.GPS_PROVIDER);
+			location.setLatitude(0.0);
+			location.setLongitude(0.0);
+			location = getLocation(location, 0);
+			if(location == null){
+				//Check if they listened, and changed got their location
+				location = getLocation(location, 1);
+			}
+			String symbol = "$";
+			double lat = 0.0;
+			double lng = 0.0;
+			//If location is null, just use default $. Otherwise, find the currency using location
+			if(location != null) {
+				lat = location.getLatitude();
+				lng = location.getLongitude();
+				try {
+					symbol = getCurrency(lat, lng);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return symbol;
+		}
+
+		public Location getLocation(Location loc, int flag) {
+			if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+				if(flag==0) {
+					//If we can't get the location, ask for it
+					new AlertDialog.Builder(mContext).setMessage("Please enable GPS")
+							.setPositiveButton("Open your location settings", new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+									mContext.startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+								}
+							})
+							.setNegativeButton("Cancel", null).show();
+				}
+				return null;
+			}else{
+				locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, locationListenerGPS);
+				loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			}
+			return loc;
+		}
+
+		//https://stackoverflow.com/questions/42218419/how-do-i-implement-the-locationlistener
+		LocationListener locationListenerGPS = new LocationListener() {
+			@Override
+			public void onLocationChanged(android.location.Location location) {
+				double latitude = location.getLatitude();
+				double longitude = location.getLongitude();
+				String msg = "New Latitude: " + latitude + "New Longitude: " + longitude;
+				Toast.makeText(mContext, msg, Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+
+			}
+		};
+
+
+
+		//Used to get the currency symbol from the latitude and longitude
+		public String getCurrency(double lat, double lng) throws IOException {
+			String countryName = getCountry(lat, lng);
+			String countryCode = getCountryCode(countryName);
+			//Create locale, get the currency, and get the symbol for that currency
+			Locale locale = new Locale("", countryCode);
+			Currency currency = Currency.getInstance(locale);
+			String symbol = currency.getSymbol();
+			return symbol;
+		}
+
+
+		//Used to get the country from latitude and longitude
+		//https://stackoverflow.com/questions/9409195/how-to-get-complete-address-from-latitude-and-longitude
+		public String getCountry(double lat, double lng) throws IOException {
+			Geocoder geocoder;
+			List<Address> addresses;
+			geocoder = new Geocoder(getActivity(), Locale.getDefault());
+			addresses = geocoder.getFromLocation(lat, lng,1);
+			return addresses.get(0).getCountryName();
+		}
+
+
+		//Used to get the country code from latitude and longitude
+		//https://stackoverflow.com/questions/28503225/get-country-code-from-country-name-in-android
+		public String getCountryCode(String countryName) {
+
+			// Get all country codes in a string array.
+			String[] isoCountryCodes = Locale.getISOCountries();
+			Map<String, String> countryMap = new HashMap<>();
+			Locale locale;
+			String name;
+
+			// Iterate through all country codes:
+			for (String code : isoCountryCodes) {
+				// Create a locale using each country code
+				locale = new Locale("", code);
+				// Get country name for each code.
+				name = locale.getDisplayCountry();
+				// Map all country names and codes in key - value pairs.
+				countryMap.put(name, code);
+			}
+
+			// Return the country code for the given country name using the map.
+			// Here you will need some validation or better yet
+			// a list of countries to give to user to choose from.
+			return countryMap.get(countryName); // "NL" for Netherlands.
+		}
+	}
